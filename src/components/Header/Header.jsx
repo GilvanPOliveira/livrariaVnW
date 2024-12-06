b4f759be2651660dbce7ffe0a61bb7560799190a
Gilvan Oliveira <gilvanoliveira06@gmail.com>
2024-12-06 12:58:42 -0300

    :iphone: Ajustando o responsivo do site

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,24 +6,38 @@ const Header = () => {
   return (
     <header className={S.boxHeader}>
       <div className={S.boxLogo}>
-        <img src={livrariaLogo} alt="Logo Livraria Vai na Web" title="Livros Vai na Web"/>
+        <img
+          src={livrariaLogo}
+          alt="Logo Livraria Vai na Web"
+          title="Livros Vai na Web"
+        />
         <h1 title="Desafio Empower - Vai na Web">Livros Vai na Web</h1>
       </div>
       <nav className={S.boxMenu}>
         <ul>
           <li>
-            <a href="/" title="Home">Home</a>
+            <a href="/" title="Home">
+              Home
+            </a>
           </li>
           <li>
-            <a href="/livros-doados" title="Livros Doados">Livros Doados</a>
+            <a href="/livros-doados" title="Livros Doados">
+              Livros Doados
+            </a>
           </li>
           <li>
-            <a href="/doacao" title="Doação">Doação</a>
+            <a href="/doacao" title="Doação">
+              Doação
+            </a>
           </li>
         </ul>
       </nav>
       <div className={S.boxBusca}>
-        <input type="text" placeholder="O que você procura?" title="Digite o livro que deseja buscar"/>
+        <input
+          type="text"
+          placeholder="O que você procura?"
+          title="Digite o livro que deseja buscar"
+        />
         <img src={busca} alt="Lupa para buscar" />
       </div>
     </header>
